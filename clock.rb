77b1a6bc7ced710631924b974613f8d23fd37208
Viktor Vsk <me@viktorvsk.com>
2021-02-16 15:35:14 +0200

    Update old effective (friendly) ads more frequently

--- clock.rb
+++ clock.rb
@@ -7,5 +7,5 @@ Corona.mount_auto_loader!
 module Clockwork
   every(1.hour, 'Crawl auto.ria.com') { AutoRia.crawl! }
   every(1.second, 'Scrape auto.ria.com') { AutoRia.scrape!(0, 2) }
-  every(1.minute, 'Actualize auto.ria.com') { AutoRia.actualize! }
+  every(12.seconds, 'Actualize auto.ria.com') { AutoRia.actualize! }
 end
