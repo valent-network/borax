ba0d9fe122482290db898fec7ca459a378c98f25
Viktor Vsk <me@viktorvsk.com>
2021-02-17 12:44:22 +0200

    Increase parsing throughput

--- clock.rb
+++ clock.rb
@@ -6,6 +6,6 @@ Corona.mount_auto_loader!
 
 module Clockwork
   every(1.hour, 'Crawl auto.ria.com') { AutoRia.crawl! }
-  every(1.second, 'Scrape auto.ria.com') { AutoRia.scrape!(0, 2) }
-  every(12.seconds, 'Actualize auto.ria.com') { AutoRia.actualize! }
+  every(1.second, 'Scrape auto.ria.com') { AutoRia.scrape!(0, 4) }
+  every(6.seconds, 'Actualize auto.ria.com') { AutoRia.actualize! }
 end
