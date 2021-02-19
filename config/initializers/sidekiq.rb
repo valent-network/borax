6a62e433a7dc1796996c12499569d682b7e69cc9
Viktor Vsk <me@viktorvsk.com>
2021-02-19 12:47:04 +0200

    Fix Redis connection

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{ENV['REDIS_SERVICE_HOST']}:6379" }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ENV['REDIS_SERVICE_HOST']}:6379" }
+end
