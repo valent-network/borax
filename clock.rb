29c624c1addcc9cd8e3fe414d0b0b5b965273146
Viktor Vsk <me@viktorvsk.com>
2021-05-10 14:01:34 +0300

    Actualize faster

--- clock.rb
+++ clock.rb
@@ -5,5 +5,5 @@ require './config/application'
 module Clockwork
   every(1.hour, 'Crawl auto.ria.com') { AutoRia::CrawlerWorker.perform_async }
   every(10.second, 'Scrape auto.ria.com') { AutoRia::Scraper.perform_async }
-  every(6.seconds, 'Actualize auto.ria.com') { AutoRia::Actualizer.perform_async }
+  every(3.seconds, 'Actualize auto.ria.com') { AutoRia::Actualizer.perform_async }
 end
