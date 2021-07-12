99989ce16b9a5047a2369d168bb02b99952ba519
Viktor Vsk <me@viktorvsk.com>
2021-07-12 11:00:08 +0300

    More freq

--- clock.rb
+++ clock.rb
@@ -4,6 +4,6 @@ require './config/application'
 
 module Clockwork
   every(1.hour, 'Crawl auto.ria.com') { AutoRia::CrawlerWorker.perform_async }
-  every(10.second, 'Scrape auto.ria.com') { AutoRia::Scraper.perform_async }
-  every(10.seconds, 'Actualize auto.ria.com') { AutoRia::Actualizer.perform_async }
+  every(5.second, 'Scrape auto.ria.com') { AutoRia::Scraper.perform_async }
+  every(5.seconds, 'Actualize auto.ria.com') { AutoRia::Actualizer.perform_async }
 end
