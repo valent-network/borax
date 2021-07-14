f15325618f105e13dc094252f7903959cf417130
Viktor Vsk <me@viktorvsk.com>
2021-07-14 20:22:20 +0300

    More urls per scrape job

--- app/workers/auto_ria/scraper.rb
+++ app/workers/auto_ria/scraper.rb
@@ -4,7 +4,7 @@ module AutoRia
     sidekiq_options queue: 'provider-auto-ria-scraper', retry: false, backtrace: false
 
     def perform
-      AutoRia.scrape!(limit: 10)
+      AutoRia.scrape!(limit: 100)
     end
   end
 end
