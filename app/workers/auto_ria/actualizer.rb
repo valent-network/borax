cb2e53ab3d9b4a7b8ce31e73b858e752f5505fb0
Viktor Vsk <me@viktorvsk.com>
2021-08-08 11:24:48 +0300

    Update actualizing ads to in_progress and not import new addresses

--- app/workers/auto_ria/actualizer.rb
+++ app/workers/auto_ria/actualizer.rb
@@ -6,14 +6,7 @@ module AutoRia
 
     def perform(addresses)
       addresses = JSON.parse(addresses)
-
-      existing_urls = Url.where(address: addresses)
-      existing_urls_addresses = existing_urls.select(:address).map(&:address)
-      addresses_to_persist = addresses - existing_urls_addresses
-
-      Url.import(%i[address status source created_at updated_at], addresses_to_persist.map { |address| [address, 'pending', AutoRia::PROVIDER, Time.now, Time.now] })
-      existing_urls.update(status: 'pending')
-
+      Url.where(address: addresses).update(status: 'in_progress')
       Url.where(address: addresses).each { |url| AutoRia::Processor.perform_async(url.id) }
     end
   end
