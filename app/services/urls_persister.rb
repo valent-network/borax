70dc80e6d5ea9d0a294297cf579025e4f01b68d7
Viktor Vsk <me@viktorvsk.com>
2021-08-08 11:41:01 +0300

    Persis crawled urls as in_progress and put them into queue with 12 hours delay

--- app/services/urls_persister.rb
+++ app/services/urls_persister.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/integer/time'
+
 class UrlsPersister
   def call(ids)
     urls = ids.map { |id| "https://auto.ria.com/auto_title_id_#{id}.html" }
@@ -5,9 +7,9 @@ class UrlsPersister
     existing_urls_addresses = existing_urls.select(:address).map(&:address)
     urls_to_persist = urls - existing_urls_addresses
 
-    Url.import(%i[address status source created_at updated_at], urls_to_persist.map { |url| [url, 'pending', AutoRia::PROVIDER, Time.now, Time.now] })
-    existing_urls.update(status: 'pending')
+    Url.import(%i[address status source created_at updated_at], urls_to_persist.map { |url| [url, 'in_progress', AutoRia::PROVIDER, Time.now, Time.now] })
+    existing_urls.update(status: 'in_progress')
 
-    Url.where(address: urls).each { |url| AutoRia::Processor.perform_async(url.id) }
+    Url.where(address: urls).each { |url| AutoRia::Processor.perform_in(12.hours, url.id) }
   end
 end
