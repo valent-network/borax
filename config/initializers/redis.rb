4017f3d08593e745303548335483fbd39b9b8d3a
Viktor Vsk <me@viktorvsk.com>
2022-11-13 00:02:08 +0100

    Fix Redis password init

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,8 @@
-REDIS = Redis.new(
+redis_params = {
   host: ENV.fetch('REDIS_SERVICE_HOST', 'localhost'),
   port: ENV.fetch('REDIS_SERVICE_PORT', '6379')
-)
+}
+
+redis_params[:password] = ENV.fetch('REDIS_SERVICE_PASSWORD') if ENV['REDIS_SERVICE_PASSWORD']
+
+REDIS = Redis.new(redis_params)
