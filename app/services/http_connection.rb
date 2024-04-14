5243e5b14ae1f44824c9dd55b2f1e27f01b8199d
Viktor Vsk <me@viktorvsk.com>
2024-04-14 21:54:58 +0200

    Use random user agents

--- app/services/http_connection.rb
+++ app/services/http_connection.rb
@@ -1,9 +1,6 @@
+# frozen_string_literal: true
+
 class HttpConnection
-  REQUEST_HEADERS = {
-    "User-Agent" => "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
-    "Accept-Language" => "en,ru-RU;q=0.8,ru;q=0.5,en-US;q=0.3",
-    "Cookie" => "lang_id=2; lang_code=ru; lang_code=ru"
-  }.freeze
   REQUEST_OPTIONS = {}.freeze
   attr_reader :connection, :last_response
 
@@ -18,16 +15,35 @@ class HttpConnection
   end
 
   def get(url)
-    @last_response = connection.get(url, REQUEST_OPTIONS, REQUEST_HEADERS)
+    @last_response = connection.get(url, REQUEST_OPTIONS, request_headers)
 
     {
       status: last_response.status,
       body: last_response.body,
       json: begin
         JSON.parse(last_response.body)
-      rescue
+      rescue StandardError
         nil
       end
+    }
+  end
+
+  private
+
+  def request_headers
+    ua = %w[
+      Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/58.0.3029.110 Safari/537.36
+      Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0
+      Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0; Trident/5.0)
+      Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; MDDCJS)
+      Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393
+      Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)
+    ]
+
+    {
+      'User-Agent' => ua.sample,
+      'Accept-Language' => 'en,ru-RU;q=0.8,ru;q=0.5,en-US;q=0.3',
+      'Cookie' => 'lang_id=2; lang_code=ru; lang_code=ru'
     }
   end
 end
